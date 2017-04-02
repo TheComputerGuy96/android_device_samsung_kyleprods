ecd7a7d8609688fdf1ce8426cfa5079c3b6346ff
Real man born on semtember <hieunam03@gmail.com>
2017-04-02 23:19:49 +0700

    Update

--- full_kyleprods.mk
+++ full_kyleprods.mk
@@ -14,11 +14,11 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+-# Inherit some common CM stuff.
+-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-# Inherit from a6000 device
-$(call inherit-product, device/lenovo/kylepods/device.mk)
+# Inherit from kyleprods device
+$(call inherit-product, device/samsung/kylepods/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleprods
